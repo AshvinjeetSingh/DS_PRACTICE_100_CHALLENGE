e111e4ac1f8617a96399e50531e3bbb6284657a4
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-04-28 12:42:16 -0400

    Move zeroes to end brute force approach

---
+++ 26_Apr_2024/Move_zeroes_to_end/brute.py
@@ -0,0 +1,25 @@
+def  moveZeroesToEnd(arr,n):
+	temp=[]
+	for item in arr:
+		if item!=0:
+			temp.append(item)
+
+	for item in range(n-len(temp)):
+		temp.append(0)
+
+	return temp
+	pass
+
+if __name__ == "__main__":
+	arr=[1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
+	n=len(arr)
+	finalArray=moveZeroesToEnd(arr,n)
+	for item in finalArray:
+		print(item," ",end="")
+
+
+Time complexity:O(n)
+Space Complexity:O(n)
+
+
+
