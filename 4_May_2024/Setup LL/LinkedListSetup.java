3fbc9e5f2b86404ba9a13646149a435f1add3efd
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-05-09 14:19:44 -0400

    Learning Linked List

--- 4_May_2024/Setup LL/LinkedListSetup.java
+++ 4_May_2024/Setup LL/LinkedListSetup.java
@@ -10,9 +10,37 @@ class Node {
 
 public class LinkedListSetup {
 
+    // code to convert arr to LL
+    private static Node convertArrToLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover=head;
+        for(int i=1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+
+        return head;
+    };
+
+    private static void traverseLL(int[] arr){
+        Node head = convertArrToLL(arr);
+        Node mover=head;
+        int cnt=0;
+        while (mover!=null){
+            System.out.println(mover.data);
+            mover=mover.next;
+            cnt++;
+        }
+        System.out.println("cnt is "+cnt);
+    };
+
+    // main function
    public static void main(String[] args) {
       int[] intArray = {1,2,3,4,5,6,7,8,9,10};
       Node text= new Node(intArray[1]);
-      System.out.println(text.data);
+      Node ans=convertArrToLL(intArray);
+      traverseLL(intArray);
+      System.out.println(ans.data);
    }
 }
